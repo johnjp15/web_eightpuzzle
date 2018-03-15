eb711015bfb1117c07f5b42446662ed0eaed9e86
johnjp15 <johnjp15@gmail.com>
2018-03-15 00:47:33 -0400

    Kind of working version...

--- PuzzleNode.js
+++ PuzzleNode.js
@@ -1,12 +1,13 @@
 class PuzzleNode
 {
 	// var priority, moves, prevState, state, prevNode;
-	constructor(priority, moves, prevState, state, prevNode)
+	constructor(priority, moves, prevState, state, prevNode, prevMoveDirection)
 	{
 		this.priority = priority;
 		this.moves = moves;
 		this.prevState = prevState;
 		this.state = state;
 		this.prevNode = prevNode;
+		this.prevMoveDirection = prevMoveDirection;
 	}
 }
